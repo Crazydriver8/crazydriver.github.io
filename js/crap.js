49c5caab68ccec1dc9f0a271ffbacdd4336a9972
Crazydriver8 <krazydriver8@gmail.com>
2017-03-18 00:32:24 -0400

    PARTY MODE 0.2
    MOAR PARTY MOAR BUTT

--- js/crap.js
+++ js/crap.js
@@ -44,18 +44,75 @@ $(document).ready(function() {
 	$("#result").val(modAge + " years old");
   });
   
-  var box = document.getElementById('box'),
-    colors = ['purple', 'yellow', 'orange', 'cyan', 'pink', 'purple', 'yellow', 'orange', 'cyan', 'pink', 'purple', 'yellow', 'orange', 'cyan', 'pink', 'purple', 'yellow', 'orange', 'cyan', 'pink'];
+  var colors = ['purple', 'yellow', 'orange', 'cyan', 'pink', 'purple', 'yellow', 'orange', 'cyan', 'pink', 'purple', 'yellow', 'orange', 'cyan', 'pink', 'purple', 'yellow', 'orange', 'cyan', 'pink', 'purple', 'orange', 'cyan', 'pink'];
   $('#witchLucky').click(function() {
+	if (!au.paused) return;
 	au.play();
+	var oldVal = document.getElementById("result").value;
+	if (oldVal == null) oldVal = "";
+	var woop = 0;
+	var i = 0;
 	(function loop(){
+		if (i > colors.length + 1) {
+			$("#originYear").val("");
+			$("#destinationYear").val("");
+			$("#modYear").val("");
+			$("#result").val("");
+		}
+		
         var color = colors.shift();
-
+		
         document.body.style.backgroundColor = color;
-
         if (colors.length) {
             setTimeout(loop, 500);
+			$("#originYear").val("");
+			$("#destinationYear").val("");
+			$("#modYear").val("");
+			$("#result").val("");
         }
+		switch (woop) {
+			case 0:
+				$("#originYear").val("FART");
+				woop++;
+				break;
+			case 1:
+				$("#destinationYear").val("FART");
+				woop++;
+				break;
+			case 2:
+				$("#modYear").val("FART");
+				woop++;
+				break;
+			case 3:
+				$("#result").val("FART");
+				woop++;
+				break;
+			case 4:
+				$("#originYear").val("BUTT");
+				woop++;
+				break;
+			case 5:
+				$("#destinationYear").val("BUTT");
+				woop++;
+				break;
+			case 6:
+				$("#modYear").val("BUTT");
+				woop++;
+				break;
+			case 7:
+				$("#result").val("BUTT");
+				woop = 0;
+				break;
+			default:
+				break;
+		}
+		i++;
+		
     })();
+	$("#originYear").val("");
+	$("#destinationYear").val("");
+	$("#modYear").val("");
+	$("#result").val("");
   });
+  
 });
