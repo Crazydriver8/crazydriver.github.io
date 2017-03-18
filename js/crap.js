8dbfb45a50847675c80b5d89bb8882692750343d
Crazydriver8 <krazydriver8@gmail.com>
2017-03-17 23:36:18 -0400

    Added mod age calculator

--- js/crap.js
+++ js/crap.js
@@ -1,4 +1,4 @@
-var main = function() {
+$(document).ready(function() {
   /* Push the body and the nav over by 285px over */
   $('.icon-menu').click(function() {
     $('.menu').animate({
@@ -22,7 +22,7 @@ var main = function() {
   });
   
   /* Set up audio playback */
-  var audio = new Audio('/content/doot doot.mp3');
+  var audio = new Audio('../content/doot_doot.mp3');
   $('.hella').click(function() {
 		audio.play();
   });
@@ -31,7 +31,18 @@ var main = function() {
   $('#skeltal').click(function() {
 		
   });
-};
-
-
-$(document).ready(main);
+  
+  $('#calcButton').click(function() {
+	alert("WHAT");
+	var origin = document.getElementById("originYear").value;
+	var destination = document.getElementById("destinationYear").value;
+	var mod = document.getElementById("modYear").value;
+	alert("Obtained: " + origin + ", " + destination + ", " + mod);
+	var normalAge = destination - origin;
+	alert("In human years: " + normalAge);
+	var modAge = normalAge / mod;
+	alert("Mod Age: " + modAge);
+	
+	$("#result").val(modAge + " years old");
+  });
+});
